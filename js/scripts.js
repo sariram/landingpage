4385b8748f907119d1c56f878921e62073b34fa1
SariRam <dh_i6anzh@sariram.me>
2021-01-23 21:57:51 -0500

    JS Edit

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ const selectElement = function (element) {
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
+
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
